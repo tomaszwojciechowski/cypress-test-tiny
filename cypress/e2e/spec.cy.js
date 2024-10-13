57f5211a2fe840cc5a154f508b15c77c50588353
Tomasz Wojciechowski <twojciechowski@protonmail.com>
2024-10-13 20:59:59 +0200

    test with KafkaJS

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,27 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+const {Kafka} = require('kafkajs');
+
+describe('describe', () => {
+  it('doesn not work in Cypess, but works in Mocha itself', async function () {
+
+    const kafka = new Kafka({
+      brokers: ['broker1:9092'],
+    });
+
+    const producer = kafka.producer();
+
+    await producer.connect();
+
+    await producer.send({
+      topic: 'topic1',
+      messages: [
+        {
+          key: '1',
+          value: 'a'
+        },
+      ],
+    });
+
+    await producer.disconnect()
   })
 })
